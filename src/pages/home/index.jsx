7f87fce01a6c807da6b0c0c7b542cc9017f21734
unknown <you@example.com>
2021-08-14 02:17:32 +0100

    Criando botão de ação (FAB) 🎬

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
-
+import { Fab } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 import List from "./components/List";
 
 function Home() {
   const [tasks, setTasks] = useState([]);
 
-  return <List tasks={tasks} />;
+  return (
+    <div class="container">
+      <List tasks={tasks} />
+      <Fab
+        color="primary"
+        style={{ position: "absolute", bottom: "30px", right: "10px" }}
+      >
+        <Add />
+      </Fab>
+    </div>
+  );
 }
 
 export default Home;
